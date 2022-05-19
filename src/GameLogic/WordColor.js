44bad4b526f589ecf37a93d0a58493347702480b
OriginalCube <lancemiraflorcube@gmail.com>
2022-05-19 23:23:29 +0800

    Update Check

--- src/GameLogic/WordColor.js
+++ src/GameLogic/WordColor.js
@@ -2,11 +2,25 @@ import React from 'react'
 
 const WordColor = () => {
     const [score, setScore] = React.useState(0);
-  return (
+    const [color, setColor] = React.useState(['#F6ED60', '#FBA51A', '#F4EB20', '#70C055', '#40B8EA', '#40B8EA', '#EC197A']);
+    const [colorText, setColorText] = React.useState(['Red', 'Orange' , 'Yellow', 'Green', 'Blue', 'Violet', 'Pink']);
+    const questionLogic = () =>{
+
+    }
+    return (
     <div className='wordColor'>
-        <div className='wordQuestion'>Blue</div>
+        <div className='wordQuestion' style={{color: color[3]}}>{colorText[2]}</div>
         <p className='wordScore'>Score : {score}</p>
-        <div className='wordAnswer'></div>
+        <div className='wordAnswer'>
+          <div className='wordBlock' style={{}}>
+            <p className='wordText'>Red</p></div>
+          <div className='wordBlock' style={{}}>
+            <p className='wordText'>Blue</p></div>
+          <div className='wordBlock' style={{}}>
+            <p className='wordText'>Green</p></div>
+          <div className='wordBlock' style={{}}>
+            <p className='wordText'>Yellow</p></div>
+        </div>
     </div>
   )
 }
